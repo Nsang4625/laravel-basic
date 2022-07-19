08b8afa0eb694e861fca705f83ddd9c0512f1fe2
Sanguyen.24 <nsang4625@gmail.com>
2022-07-19 20:01:37 +0700

    Initial commit

---
+++ resources/views/home/contact.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('title', 'Contact page')
+@section('content')
+<h1>Contact me here: 0818623567</h1>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home.index', []);
+}) -> name('home.index');
+
+Route::get('/contact', function(){
+    return view('home.contact');// view's name is alike source to blade file
+}) -> name('home.contact');// should named like view
+
+Route::get('/post/{id}', function($id){
+    $posts = [
+        1 => [
+            'title' => 'Intro to laravel',
+            'content' => 'Welcome to laravel 8'
+        ],
+        2 => [
+            'title' => 'Intro to php',
+            'content' => 'Welcome to php 8.1.7'
+        ]
+    ];
+    abort_if(!isset($posts['id']), 404);
+    return view('posts.show', ['post' => $posts['id']]);// gán $posts['id] cho post
+    // post được chuyển sang blade trở thành array 
+}) -> name('posts.show');
+// add constraint for required parameter
+// -> where([
+//     'id' => '[0-9]+'
+// ])
+// define constraint for id globaly in function boot inside file App/provider/RouteServiceProvider.php
+
+// compulsory parameter
+
+Route::get('/recent-post/{days_Ago?}', function($daysAgo = 20){
+    return 'Posts from ' . $daysAgo . ' days ago';
+}) -> name('posts.recent.index');
+// optional parameter
