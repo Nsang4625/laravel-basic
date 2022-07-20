1cbbe21d8547d565534f8452232393e325569abd
Sanguyen.24 <nsang4625@gmail.com>
2022-07-20 23:33:53 +0700

    Create index.blade.php

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title','Blog')
+@section('content')
+{{-- @foreach ($posts as $key => $post)
+    <div>{{ $key }}.{{ $post['content'] }}</div>
+@endforeach --}}
+@forelse ($posts as $key => $post)
+    {{-- @break($key == 1) --}}
+    @include('posts.partial.post')
+@empty {{-- this is a combine of foreach and empty(array) --}}
+    <div>There's no post</div>
+@endforelse
+{{-- @each($source,$data ,$variable ) ~ @forelse + @include
+    variable is which data converted to --}}
