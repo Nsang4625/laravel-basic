71e5644e9592cf37e35cd7c74b619f6650724ae2
Sanguyen.24 <nsang4625@gmail.com>
2022-07-21 23:32:17 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,26 +26,21 @@ Route::get('/contact', function(){
 }) -> name('home.contact');// should named like view
 */
 // if you don't need to pass data or parameter, you can use view method like:
-Route::view('/', 'home.index')
-->name('home.index');
-Route::view('/contact', 'home.contact')
-->name('home.contact');
+// Route::view('/', 'home.index')
+// ->name('home.index');
+// Route::view('/contact', 'home.contact')
+// ->name('home.contact');
+Route::get('/', [HomeController::class, 'home'])
+    ->name('home.index');
+Route::get('/contact', [HomeController::class, 'contact'])
+    ->name('home.contact');
+Route::get('/single', AboutController::class);
+// with single action controller like above
+//we don't need to pass the name function
 
-$posts = [
-    1 => [
-        'title' => 'Intro to laravel',
-        'content' => 'Welcome to laravel 8',
-        'is_new' => true,
-        'has_comment' => true
-    ],
-    2 => [
-        'title' => 'Intro to php',
-        'content' => 'Welcome to php 8.1.7',
-        'is_new' => false
-    ]
-];
+
 // to use $posts below, we must spend 'use' statement
-
+/*
 Route::get('/posts', function() use($posts){
     request()->all();// all method get all data in input and return an array 
     request()->input('post', 1);// get value of 'post' in input, default is 1
@@ -55,43 +53,47 @@ Route::get('/post/{id}', function($id) use($posts){
     return view('posts.show', ['post' => $posts['id']]);// gán $posts['id] cho post
     // post được chuyển sang blade trở thành array 
 }) -> name('posts.show');
+*/
 // add constraint for required parameter
 // -> where([
 //     'id' => '[0-9]+'
 // ])
 // define constraint for id globaly in function boot inside file App/provider/RouteServiceProvider.php
-
 // compulsory parameter
-
-Route::get('/recent-post/{days_Ago?}', function($daysAgo = 20){
+Route::resource('posts', PostsController::class)
+->only(['index', 'show']);//give only use 2 methods
+//in another way except() method will disable those methods
+Route::get('/recent-post/{days_Ago?}', function ($daysAgo = 20) {
     return 'Posts from ' . $daysAgo . ' days ago';
-}) -> name('posts.recent.index');
+})->name('posts.recent.index');
 // optional parameter
-
-Route::get('/fun/response',function() use($posts){
-    return response($posts, 201) // this obj also has view method 
-    //that return blade
-    -> header('Content-type', 'application/json') 
-    ->cookie('MY_COOKIE', 'Sang', 3600);
-});
+/*
+Route::get('/fun/response', function () use ($posts) {
+    return response($posts, 201) // response obj also has view method 
+        //that return blade: ->view(...)
+        ->header('Content-type', 'application/json')
+        ->cookie('MY_COOKIE', 'Sang', 3600);
+});*/
 // we can group routes which have the same prefix name
-Route::prefix('/fun')->name('fun.')->group(function()use($posts){
-Route::get('/redirect', function(){
-    return redirect('/contact');
- });
-Route::get('/back', function(){
-    return back();// redirect to the address right before
- });
-Route::get('/named-route', function(){ 
-    return redirect()->route('home.index');// redirect to a route name
+/*
+Route::prefix('/fun')->name('fun.')->group(function () use ($posts) {
+    Route::get('/redirect', function () {
+        return redirect('/contact');
+    });
+    Route::get('/back', function () {
+        return back(); // redirect to the address right before
+    });
+    Route::get('/named-route', function () {
+        return redirect()->route('home.index'); // redirect to a route name
+    });
+    Route::get('/google', function () {
+        return redirect()->away('https://google.com'); // redirect to another web page
+    });
+    Route::get('/jason', function () use ($posts) {
+        return response()->json($posts);
+    });
+    Route::get('/download', function () {
+        return response()->download(public_path('/Quynh.jpg'), 'hotgirl.jpg');
+    });
 });
-Route::get('/google', function(){
-    return redirect()->away('https://google.com');// redirect to another web page
-});
-Route::get('/jason', function() use($posts){
-    return response()->json($posts);
-});
-Route::get('/download', function(){
-    return response()->download(public_path('/Quynh.jpg'), 'hotgirl.jpg');
-});
-});
+*/
