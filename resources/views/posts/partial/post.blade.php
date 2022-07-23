39b61cb08b12523d655b2f338f1df52ba30c7d34
Sanguyen.24 <nsang4625@gmail.com>
2022-07-24 00:10:47 +0700

    Update post.blade.php

--- resources/views/posts/partial/post.blade.php
+++ resources/views/posts/partial/post.blade.php
@@ -1,9 +1,16 @@
 @if ($loop->even){
     <h1>This is even post</h1>
-    <div>{{ $key }}.{{ $post['title'] }}</div>
+    <div>{{ $key }}.{{ $post -> title }}</div>
 }
 @else
     <div style="background-color: rgb(124, 60, 60)">
-    {{ $key }}.{{ $post['title'] }}
+    {{ $key }}.{{ $post -> content }}
     </div>
 @endif
+<div>
+    <form action="{{ route('home.contact', ['post' => $post -> id]) }}">
+        @method('DELETE')
+        @csrf
+        <input type="submit" value="DELETE!">
+    </form>
+</div>
