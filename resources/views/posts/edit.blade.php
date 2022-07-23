fe2f6c8cad838d7644a9e23fc0db47ec98fd5e77
Sanguyen.24 <nsang4625@gmail.com>
2022-07-24 00:10:57 +0700

    Create edit.blade.php

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('title', 'Update')
+@section('content')
+<form action="{{ route('posts.update'), ['post' => $post -> id] }}" method="POST">
+    @csrf
+    @method('PUT')
+    @include('posts.partial.form')
+    <div><input type="submit" value="Update"></div>
+</form>
+@endsection
