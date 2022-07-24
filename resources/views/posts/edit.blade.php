6b4356df8c4d94a580e43c234b2d4be6f433ff91
Sanguyen.24 <nsang4625@gmail.com>
2022-07-24 15:11:19 +0700

    Update edit.blade.php

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('title', 'Update')
 @section('content')
-<form action="{{ route('posts.update'), ['post' => $post -> id] }}" method="POST">
+<form action="{{ route('posts.update', ['post' => $post -> id]) }}" method="POST">
     @csrf
     @method('PUT')
     @include('posts.partial.form')
