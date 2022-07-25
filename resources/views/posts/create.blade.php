3624fea4dcafa00aba197200479274455c1e2747
Sanguyen.24 <nsang4625@gmail.com>
2022-07-25 22:58:56 +0700

    Update create.blade.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,6 +4,6 @@
 <form action="{{ route('posts.store') }}" method="POST">
     @csrf
     @include('posts.partial.form')
-    <div><input type="submit" value="Create"></div>
+    <div><input type="submit" value="Create" class="btn btn-primary btn-block"></div>
 </form>
 @endsection
