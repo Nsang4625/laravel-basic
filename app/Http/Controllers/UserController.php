5432a5a9fc6350778fa74625720406f8fccba301
Sanguyen.24 <nsang4625@gmail.com>
2022-08-29 23:05:39 +0700

    33.3.1 creating views

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -51,7 +51,7 @@ class UserController extends Controller
      */
     public function show(User $user)// to pass this model automaticaly with id in route
     {
-        //
+        return view('users.show', ['user' => $user]);
     }
 
     /**
@@ -62,7 +62,7 @@ class UserController extends Controller
      */
     public function edit(User $user)
     {
-        //
+        return view('users.edit', ['user' => $user]);
     }
 
     /**
