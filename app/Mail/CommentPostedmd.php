d0e15aae962de8f09e8dc163b0b59297cf085e66
Sanguyen.24 <nsang4625@gmail.com>
2022-09-10 15:16:02 +0700

    37.2 Configuring queues, creating and running jobs

--- app/Mail/CommentPostedmd.php
+++ app/Mail/CommentPostedmd.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class CommentPostedmd extends Mailable
+class CommentPostedmd extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
     public $comment;
